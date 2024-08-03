d3144c47431bbd82c2d9e71e7cd63acfbc39b61c
daguenette <da.guenette@icloud.com>
2024-08-03 18:10:16 -0400

    Added state for form with session manager

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,11 +12,12 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", app.homeHandler)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetViewHandler)
-	mux.HandleFunc("GET /snippet/create", app.snippetFormHandler)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreateHandler)
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.homeHandler))
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetViewHandler))
+	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetFormHandler))
+	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreateHandler))
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 
 	return standard.Then(mux)
